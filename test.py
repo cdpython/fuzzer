85c0ec5f7dd26fa232ff5c767d62b9cc3e7d6c27
cdpython <candypython@gmail.com>
2012-10-30 20:32:49 +0900

    pid 입력후 레지스터 출력
    레지스터 출력 기능 추가 및 debugger 오류 제거

--- test.py
+++ test.py
@@ -2,4 +2,24 @@ import debugger
 
 debugger = debugger.debugger()
 
-debugger.load("C:\\WINDOWS\\system32\\calc.exe")
+pid = raw_input("PID : ")
+
+debugger.attach(int(pid))
+
+lists = debugger.enumerate_threads()
+
+for thread in lists:
+
+    thread_context = debugger.get_thread_context(thread)
+
+    print "[*] Dumping registers for thread ID: 0x%08x" % thread
+    print "[**] EIP: 0x%08x" % thread_context.Eip
+    print "[**] ESP: 0x%08x" % thread_context.Esp
+    print "[**] EBP: 0x%08x" % thread_context.Ebp
+    print "[**] EAX: 0x%08x" % thread_context.Eax
+    print "[**] EBX: 0x%08x" % thread_context.Ebx
+    print "[**] ECX: 0x%08x" % thread_context.Ecx
+    print "[**] EDX: 0x%08x" % thread_context.Edx
+    print "[*] END DUMP"
+  
+debugger.detach()
