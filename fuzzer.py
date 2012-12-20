829f80a347a085671ba2caed06306df8b126e936
cdpython <candypython@gmail.com>
2012-12-20 14:17:53 +0900

    트래킹 종료 시 파일저장 관련 문제 수정, EIP기준 크래시 중복체크 추가
    1. 트래킹 종료 시 파일저장이 정상적으로 이루어 지지 않던 부분 수정
    2. EIP를 기준으로 크래시의 중복여부를 체크

--- fuzzer.py
+++ fuzzer.py
@@ -12,27 +12,28 @@ import time
 
 class file_fuzzer:
     def __init__(self, exe_path):
-        self.mutate_count       = 50
-        self.mutate_list            = []
-        self.selected_list          = [] # 크래시 트래킹에 사용할 리스트
-        self.exe_path           = exe_path
-        self.ext            = ".hwp"
-        self.orig_file          = None
-        self.sample_dir         = "sample\\"
-        self.tmp_file           = None
-        self.tmp_dir            = "tmp\\"
-        self.count          = 0
-        self.crash          = None
-        self.crash_tracking = False # 크래시 추적 활성화 체크
+        self.mutate_count        = 50
+        self.mutate_list         = []
+        self.selected_list       = [] # 크래시 트래킹에 사용할 리스트
+        self.eip_list            = []    #크래시 중복체크 (EIP 기준)
+        self.exe_path            = exe_path
+        self.ext                 = ".hwp"
+        self.orig_file           = None
+        self.sample_dir          = "sample\\"
+        self.tmp_file            = None
+        self.tmp_dir             = "tmp\\"
+        self.count               = 0
+        self.crash               = None
+        self.crash_tracking      = False # 크래시 추적 활성화 체크
         self.crash_tracking_step = 0 # 크래시 추적 단계 설정
-        self.crash_again    = False # 크래시 여부 체크
-        self.pivot            = None # 랜덤 인덱스 저장을 위한 변수
-        self.crash_count = None # 크래시 번호 저장
-        self.check          = False
-        self.pid            = None
-        self.in_accessv_handler     = False
-        self.dbg            = None
-        self.running            = False
+        #self.crash_again    = False # 크래시 여부 체크
+        self.pivot               = None # 랜덤 인덱스 저장을 위한 변수
+        self.crash_count         = None # 크래시 번호 저장
+        self.check               = False
+        self.pid                 = None
+        self.in_accessv_handler  = False
+        self.dbg                 = None
+        self.running             = False
 
     # 파일 선택
     def file_picker(self):
@@ -59,9 +60,9 @@ class file_fuzzer:
                 self.file_picker()
                 self.mutate_file()
             else: #크래시 추적이 활성화 되었으면
-                
+                print "[ * ] Crash Tracking Start !!!", self.orig_file
                 # 크래시 난 파일 복사
-                shutil.copy(self.orig_file,  self.tmp_file)
+                shutil.copy(self.orig_file, self.tmp_file)
                 # 트래킹하는 뮤테이션 함수 호출
                 self.mutate_track()
 
@@ -71,7 +72,6 @@ class file_fuzzer:
             pydbg_thread.start()
 
             while self.pid == None:
-
                 time.sleep(0.5)
 
             # 모니터링 쓰레드 실행
@@ -93,13 +93,12 @@ class file_fuzzer:
         self.pid = self.dbg.pid
         self.dbg.run()
 
-
     # 어플레킹션을 몇 초 동안 실행 되게 한 다음 종료시키는 모니터링 쓰레드 
     def monitor_debugger(self):
 
         counter = 0
         print "[*] waiting ",
-        while counter < 3:
+        while counter < 3 and pid != None:
             time.sleep(1)
             print ".",
             counter += 1
@@ -113,7 +112,6 @@ class file_fuzzer:
             
         else:
             while self.pid != None:
-                
                 time.sleep(0.5)
         
         while True :
@@ -130,12 +128,27 @@ class file_fuzzer:
     # 에러를 추적하고 정보를 저장하기 위한 접근 위반 핸들러 
     def check_accessv(self, dbg):
 
+        # 중복된 크래시 인지 체크
+        if self.dbg.context.Eip in self.eip_list:
+            print "\n[ x ] Duplicate Crash!!"
+            self.in_accessv_handler = False
+            self.dbg.terminate_process()
+            self.pid = None
+
+            return DBG_EXCEPTION_NOT_HANDLE
+
+        # eip 리스트에 추가
+        self.eip_list.append(self.dgb.context.Eip)
+        
         # 트래킹 활성화 여부 체크
         if self.crash_tracking == False:
             # 트래킹 활성화
             self.crash_tracking = True
-            print "\n[*] Woot! Handling an access violation!"
             self.in_accessv_handler = True
+            
+            print "\n[*] Woot! Handling an access violation!"
+            print "[*] EIP : 0x%08x" % self.dbg.context.Eip
+            
             crash_bin = utils.crash_binning.crash_binning()
             crash_bin.record_crash(dbg)
             self.crash = crash_bin.crash_synopsis()
@@ -147,10 +160,7 @@ class file_fuzzer:
             crash_fd.write(self.crash)
             crash_fd.write("----------------- mutate log -------------------\n")
             for i in self.mutate_list:
-                crash_fd.write("offset : ")
-                crash_fd.write(hex(i[0]))
-                crash_fd.write(", 0x"+i[1])
-                crash_fd.write("\n")
+                crash_fd.write("offset : "+ hex(i[0])+", 0x"+i[1] + "\n" )
             #crash_fd.write("\n\nEND")
             crash_fd.close()
 
@@ -164,13 +174,52 @@ class file_fuzzer:
 
         # 트래킹 활성화 시 수행할 루틴 
         else:
-            print "[+] Crash Tracking..."
-
-            self.crash_again = True
-
+            
+            #print "[+] Crash Tracking..."
+            #self.crash_again = True
+            
+            #접근위반 핸들러 활성화
+            self.in_accessv_handler = True
             self.dbg.terminate_process()
             self.pid = None
 
+            # 크래시가 발생하면 수행할 루틴
+            print "[+] crash Again!!"
+            # 크래시 난 리스트를 뮤테이션 리스트에 넣는다.
+            self.mutate_list = self.selected_list
+            # 크래시가 나면 새로운 피봇 설정
+            self.pivot = self.mutate_list.index(random.choice(self.mutate_list))
+            self.check = False
+
+            print "[+] Mutate list count -- %d" % len(self.mutate_list)
+
+             # 뮤테이션 리스트 원소의 갯수가 5개보다 적으면 수행 할 루틴
+            if len(self.mutate_list) <5:
+                print "[ ^^ ] tracking Finished! %d -> %d" % (self.mutate_count, len(self.mutate_list))
+                # 크래시 파일 백업
+                shutil.copy(self.tmp_file, "crash\\crash_%d%s" % (self.crash_count,self.ext))
+                """
+                f= open("crash\\%d%s" % (self.crash_count,self.ext),'r+b')
+                for i in self.mutate_list:
+                    f.seek(i[0])
+                    f.write(chr(int(i[1][:2],16)) * (len(i[1])/2))
+                f.close()
+                """
+                # 로그 추가 기록
+                f = open("crash\\crash-%d.log" % self.crash_count, 'a')
+                f.write("\n\n---------------- Check this Offset!! ------------------\n\n")
+                for i in self.mutate_list:
+                    f.write("offset : "+ hex(i[0])+", 0x"+i[1] + "\n" )
+                f.write("\n\nEND")
+                f.close()
+
+                # 각종 변수 초기화
+                self.crash_tracking = False
+                self.crash_again = False
+                self.crash_tracking_step = 0
+                self.selected_list = []
+                self.pivot = 0
+            
             return DBG_EXCEPTION_NOT_HANDLED
 
 
@@ -201,59 +250,12 @@ class file_fuzzer:
 
     def mutate_track( self ):
 
-        # 뮤테이션 리스트 원소의 갯수가 5개보다 적으면 수행 할 루틴
-        if len(self.mutate_list) < 5:
-            print "[ ^^ ] tracking Finished! %d -> %d" % (self.mutate_count, len(self.mutate_list))
-            # 크래시 파일 백업
-            shutil.copy(self.tmp_file, "crash\\%d%s" % (self.crash_count,self.ext))
-
-            f= open("crash\\%d%s" % (self.crash_count,self.ext),'r+b')
-            for i in self.mutate_list:
-                f.seek(i[0])
-                f.write(chr(int(i[1][:2],16)) * (len(i[1])/2))
-            f.close()
-            # 각종 변수 초기화
-            self.crash_tracking = False
-            self.crash_again = False
-            self.crash_tracking_step = 0
-            self.selected_list = []
-            self.pivot = 0
-            # 로그 추가 기록
-            f = open("crash\\crash-%d.log" % self.crash_count, 'a')
-            f.write("\n\n---------------- Check this Offset!! ------------------\n\n")
-            for i in self.mutate_list:
-                f.write("offset : ")
-                f.write(hex(i[0]))
-                f.write(", 0x"+i[1])
-                f.write("\n")
-            f.write("\n\nEND")
-            f.close()
-            
-            return
-
-
-        # 수정할 파일 오픈 
-        f = open(self.tmp_file, 'r+b')
-
         # 트래킹이 처음 스탭일때(0) 수행
         if self.crash_tracking_step == 0:
             # 랜덤한 피봇 설정
             self.pivot= self.mutate_list.index(random.choice(self.mutate_list))
             # 트래킹 스탭 1로 설정
             self.crash_tracking_step = 1
-
-        # 크래시가 발생하면 수행할 루틴
-        if self.crash_again == True:
-            print "[+] crash Again!!"
-            # 크래시 난 리스트를 뮤테이션 리스트에 넣는다.
-            self.mutate_list = self.selected_list
-            # 크래시가 나면 새로운 피봇 설정
-            self.pivot = self.mutate_list.index(random.choice(self.mutate_list))
-            self.check = False
-
-
-        print "[*] tracking Selected file : %s" % self.orig_file
-        print "[+] Mutate list %d" % len(self.mutate_list)
 
         #크래시가 안났으면 기존 피봇 사용
         pivot = self.pivot
@@ -273,14 +275,19 @@ class file_fuzzer:
             self.selected_list = right
             #체크 변수 토글
             self.check = False
+            
+        # 수정할 파일 오픈 
+        f = open(self.tmp_file, 'r+b')
         
         #tmp 파일에 쓰기
         for i in self.selected_list:
+            print i[0], i[1]
             f.seek(i[0])
             f.write(chr(int(i[1][:2],16)) * (len(i[1])/2))
         f.close()
+        
         # 크래시 체크 변수 off
-        self.crash_again = False
+        #self.crash_again = False
         return
 
 if __name__ == "__main__":
